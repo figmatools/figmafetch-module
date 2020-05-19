a577b4f6fb52638ee0590a33dbd48a11febe05c0
____marcell <0000marcell@gmail.com>
2020-05-19 00:16:01 +0000

    added suport for nodeIds fetch

--- figmafetch.js
+++ figmafetch.js
@@ -3,9 +3,9 @@ const headers = new fetch.Headers();
 
 const baseUrl = 'https://api.figma.com';
 
-module.exports = async function fetchProject(projectId, token) {
+module.exports = async function fetchProject(fileId, token, nodeIds = []) {
   headers.set('X-Figma-Token', token);
-  let resp = await fetch(`${baseUrl}/v1/files/${projectId}`, 
+  let resp = await fetch(`${baseUrl}/v1/files/${fileId}?${nodeIds.join(',')}`, 
     {headers});
   let data = await resp.json();
   return data;
--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-const fetchProject = require('./index')
-let id = 'YZkG7swHVcXXJ6gA5PfLuNzw'
+const fetchProject = require('./figmafetch')
+let fileId = 'YZkG7swHVcXXJ6gA5PfLuNzw'
 let token = '12606-e447b778-09cc-4bd3-91e8-843146de402d';
+let nodeIds = ['6:36'];
+let fs = require('fs');
+
 (async () => {
-  let data = await fetchProject(id, token);
+  let data = await fetchProject(fileId, token, nodeIds);
     data = data;
-    data['headers'] = { token: token, id: id };
-    console.log(JSON.stringify(data));
-  console.log(JSON.stringify(data));
+    data['headers'] = { token: token, id: fileId };
+  fs.writeFileSync('./test-result', JSON.stringify(data, null, 2))
 })()
 
